5a9950370e510977c553a4a30b990f9b146fa056
Sergei Kurochkin <kurochkinsergei95@gmail.com>
2018-12-02 17:12:47 +0300

    Human component

--- group_16/first_app/src/App.js
+++ group_16/first_app/src/App.js
@@ -1,6 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
 
+class Human extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.firstName = props.firstName;
+  //   this.lastName = props.lastName;
+  //   this.age = props.age;
+  // }
+
+  sayHi(city) {
+    return 'HELLO MY NAME IS - ' + this.props.firstName + ' ' + this.props.lastName + ' I am from ' + city;
+  }
+
+  render() {
+    console.log("HUMAN PROPS:", this.props);
+    
+    // то же самое, что
+    // const firstName = this.props.firstName;
+    // это называется  destructive assignment
+    const { firstName, lastName, age } = this.props;
+
+    return <div>
+        <h2>Person name: { firstName } { lastName }</h2>     
+        <h2>Person age: { age }</h2>
+        <p>
+          Greeting: { this.sayHi('Berlin') }
+        </p>     
+    </div>
+  }
+}
+
 class App extends Component {
   constructor() {
     super()
@@ -14,14 +44,23 @@ class App extends Component {
   }
 
   render() {
+    // let mike = new Human('Michael', 'Vundershaft', 57);
+    // console.log(mike);
+
+    // mike.sayHi('Berlin');
+
     return (
       <div className="counter-wrapper">
         <h1>Hello world</h1>
         <div>
+          
           {this.state.counter}
         </div>
         <button>-</button>
         <button onClick={this.incCounter}>+</button>
+
+        <Human firstName={'Ron'} lastName={'Little'} age={20} />
+        <Human firstName={'Katty'} lastName={'Middle'} age={18} />
       </div>
       )
   }
