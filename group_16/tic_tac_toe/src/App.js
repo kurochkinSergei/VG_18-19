cc24f4e0217f5366d844dfe8d50f443c2b17135c
Sergei Kurochkin <kurochkinsergei95@gmail.com>
2019-01-12 17:04:51 +0300

    tictactoe_react

---
+++ group_16/tic_tac_toe/src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import uuid4 from 'uuid4';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      cells: this.init(3)
+    }
+  }
+
+  init(size) {
+    let cells = [];
+
+    for (let i = 0; i < size*size; i++) {
+      cells.push({
+        id: uuid4(),
+        value: '',
+        order: i,
+      })
+    }
+
+    return cells;
+  }
+
+  render() {
+    return (
+        <div>
+          {JSON.stringify(this.state)}
+        </div>
+    );
+  }
+}
+
+export default App;
