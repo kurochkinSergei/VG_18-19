54eb6d1655786cab6fffd2f5475825d399966688
Sergei Kurochkin <kurochkinsergei95@gmail.com>
2019-02-02 16:30:40 +0300

    styles

--- group_16/ping-pong/src/GameField.js
+++ group_16/ping-pong/src/GameField.js
@@ -1,11 +1,49 @@
 import React, { Component } from 'react';
 
 class GameField extends Component {
+  width = 500;
+  height = 600;
+
+  state = {
+    racketOne: {
+        position: {
+            left: 200,
+            top: 0,
+        },
+        score: 0,
+    },
+    racketTwo: {
+        position: {
+            left: 200,
+            top: this.height - 20,
+        },
+        score: 0,
+    },
+
+    ball: {
+        position: {
+            top: 290,
+            left: 240,
+        }
+    }
+  }
+
   render() {
+    const racketOneStyle = {
+        left: this.state.racketOne.position.left,
+        top: this.state.racketOne.position.top,
+    } 
+
+    const racketTwoStyle = {
+        left: this.state.racketTwo.position.left,
+        top: this.state.racketTwo.position.top,
+    } 
+
+    console.log('racketStyle', racketOneStyle)
     return (
     <div className="field">
-        <div className="caret caret_top" />
-        <div id="caret-bottom" className="caret caret_bottom" />        
+        <div className="caret" style={racketOneStyle} />
+        <div className="caret" style={racketTwoStyle} />        
         <div className="ball" />
     </div>
     );
