cc948fc3decb969a7b63f2e54917c4240a90bf00
Sergei Kurochkin <kurochkinsergei95@gmail.com>
2019-02-02 17:18:48 +0300

    move rackets

--- group_16/ping-pong/src/GameField.js
+++ group_16/ping-pong/src/GameField.js
@@ -30,11 +30,39 @@ class GameField extends Component {
     }
   }
 
+  componentDidMount() {
+    document.addEventListener('keydown', this.moveRacket);
+
+    // шарик двигать через setInterval
+    // setInterval(this.moveBall, 100)
+}
+
+    // ракетка, которой нужно изменить стили, должна выбираться на основании нажатой клавиши
+  moveRacket = (e) => {
+    console.log('EVENT', e);
+
+    let racket = 'racketTwo';
+
+    if (e.key === 'ArrowRight' || e.key === 'ArrowLeft') {
+        racket = 'racketOne';
+    }
+
+    this.setState({
+        [racket]: {
+            position: {
+                left: this.state[racket].position.left + 25,
+                top: this.state[racket].position.top,
+            }
+    }})
+  }
+
   render() {
     return (
     <div className="field">
 
-        <Racket {...this.state.racketOne} />
+        <Racket
+            {...this.state.racketOne}
+        />
         
         <Racket
             position={this.state.racketTwo.position}
--- group_16/ping-pong/src/Racket.js
+++ group_16/ping-pong/src/Racket.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
 
 class Racket extends Component {
-  render() {
-    const { position } = this.props;
- 
-    console.log('RACKET PROPS', this.props);
 
-    const racketStyle = {
-        left: position.left,
-        top: position.top,
-    } 
 
-    return (
-        <div className="caret" style={racketStyle} />
-    );
-  }
+  
+    render() {
+        const { position } = this.props;
+    
+        console.log('RACKET PROPS', this.props);
+
+        const racketStyle = {
+            left: position.left,
+            top: position.top,
+        } 
+
+        return (
+            <div className="caret" style={racketStyle} />
+        );
+    }
 }
 
 export default Racket;
